d7e732f6778377a350639ec8c037fe1d99ac1dfc
Nicole Nouwels <nicole.n.nouwels@gmail.com>
2019-04-09 21:58:59 -0700

    comm-644-assignment05

--- beginning/js/calculator.js
+++ beginning/js/calculator.js
@@ -1,28 +1,42 @@
 /*eslint-env browser*/
 
-window.addEventListener('load', init);
-
-function init() {
-    'use strict';
-    var btn;
-    
-
-    document.getElementById(btn).addEventListener('click', enter)
-
- }
-
-    }
-};
 var $ = function () {
     'use strict';
     document.getElementById(id);
 };
 
+function init() {
+    'use strict';
+    $('one').addEventListener('click', enter);
+    $('two').addEventListener('click', enter);
+    $('three').addEventListener('click', enter);
+    $('four').addEventListener('click', enter);
+    $('five').addEventListener('click', enter);
+    $('six').addEventListener('click', enter);
+    $('seven').addEventListener('click', enter);
+    $('eight').addEventListener('click', enter);
+    $('nine').addEventListener('click', enter);
+    $('zero').addEventListener('click', enter);
+    $('add').addEventListener('click', enter);
+    $('subtract').addEventListener('click', enter);
+    $('multiply').addEventListener('click', enter);
+    $('divide').addEventListener('click', enter);
+    $('period').addEventListener('click', enter);
+}
+
 function enter(val) {
-    document.getElementById('result').value=val;
-    document.getElementById('result') += val;
+    var val = init.value;
+  $('result').value += val;
+}
+
+function equal() {
+    $('equal').value;
+    calculate();
+}
     
-};
- function equal(){
- enter(eval(document.getElementById('result').value));
- };
+function calculate() {
+        var newVal = $('result').value;
+        eval('newVal' + enter);
+    }
+
+window.addEventListener('load', init);
--- beginning/js/dom-practice.js
+++ beginning/js/dom-practice.js
@@ -1,33 +1,44 @@
 /*eslint-env browser*/
 
 //STEP 1
+
 /*   function myFunction() {
         window.alert("I have been cicked");
 };*/
+
 //STEP 2
+
 /*window.document.getElementById('myButton').onclick =
     alert('I have been clicked');*/
+
 //STEP 3
+
 /*  document.addEventListener('click', myFunction);
 function myFunction(){
 document.getElementById('myButton');
 window.alert('I have been clicked');
          }*/
+
 //STEP 4
+
 /*  document.addEventListener('click', function() {
     document.getElementById('myButton');
     window.alert('I have been clicked');
 
 });*/
+
 //STEP 5
 
 //STEP 6
+
 /*    document.getElementById('redirect');
     document.addEventListener('click', function() {
     alert('You have clicked this link');
     event.preventDefault()
         });*/
+
 //STEP 7
+
 /* var go = document.getElementById('go');
 var text = document.getElementById('displayText');
 text.value = 'Enter a search term';
@@ -36,6 +47,8 @@ go.addEventListener('click', function() {
     alert(text.value);
 
 });*/
+
+//STEP 8
 
 //STEP 9
 
